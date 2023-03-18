972b3c50b9a12815d9328a42515acd38454762c2
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-03-18 13:39:00 -0300

    Operador logico

---
+++ script.js
@@ -0,0 +1,24 @@
+/*
+Operadores Lógicos
+&& -> AND -> E Todas as expressoes precisam ser verdadeiras para retornar true
+|| -> OR -> OU
+! -> NOT -> NAO
+*/
+
+const expressaoAnd = true && true && true;
+console.log(expressaoAnd);
+const expressaoOr = true || false || false;
+console.log(expressaoOr);
+
+
+//EXEMPLO DO &&
+/*
+const usuario = 'Luiz';
+const senha = 123456;
+const vaiLogar = usuario === 'Luiz' && senha === 123456;
+console.log(vaiLogar);
+*/
+
+console.log(!true)
+
+
